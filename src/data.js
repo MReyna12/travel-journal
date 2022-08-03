bc260a6223f66ea50a20d1816e8f27255c4483df
Michael <MReyna12@live.com>
2022-08-03 13:15:29 -0500

    add data to be implemented with React

---
+++ src/data.js
@@ -0,0 +1,90 @@
+export default [
+  {
+    id: 1,
+    title: "Mount Rainier National Park",
+    location: "Washington",
+    googleMapsUrl: "https://goo.gl/maps/JymwQDPfsF9JdZoy5",
+    startDate: "1 Jan, 2021",
+    endDate: "1 Jan, 2021",
+    description:
+      "Mount Rainier National Park, a 369-sq.-mile Washington state reserve southeast of Seattle, surrounds glacier-capped, 14,410-ft. Mount Rainier. Atop 6,400-ft.-high Sunrise, the highest point in the park reachable by car, visitors can admire Rainier and other nearby volcanoes, including Mount Adams. The park's 5,400-ft.-high Paradise overlook offers mountain views, summertime wildflower meadows and hiking trailheads.",
+    imageUrl: "../public/yosemite.jpg",
+  },
+  {
+    id: 2,
+    title: "Miami Beach",
+    location: "Florida",
+    googleMapsUrl: "https://goo.gl/maps/a67BrRo8HNSTkMxa9",
+    startDate: "1 Jan, 2021",
+    endDate: "1 Jan, 2021",
+    description:
+      "Miami Beach is a south Florida island city, connected by bridges to mainland Miami. Wide beaches stretch from North Shore Open Space Park, past palm-lined Lummus Park to South Pointe Park. The southern end, South Beach, is known for its international cachet with models and celebrities, and its early-20th-century architecture in the Art Deco Historic district with pastel-colored buildings, especially on Ocean Drive.",
+    imageUrl: "../public/yosemite.jpg",
+  },
+  {
+    id: 3,
+    title: "Yosemite National Park",
+    location: "California",
+    googleMapsUrl: "https://goo.gl/maps/amzMpN22YSxi8btw8",
+    startDate: "1 Jan, 2021",
+    endDate: "1 Jan, 2021",
+    description:
+      "Yosemite National Park is in California's Sierra Nevada mountains. It's famed for its giant, ancient sequoia trees, and for Tunnel View, the iconic vista of towering Bridalveil Fall and the granite cliffs of El Capitan and Half Dome.",
+    imageUrl: "../public/yosemite.jpg",
+  },
+  {
+    id: 4,
+    title: "Laguna Beach",
+    location: "California",
+    googleMapsUrl: "https://goo.gl/maps/BLfMbGAbqxGGMDTX8",
+    startDate: "1 Jan, 2021",
+    endDate: "1 Jan, 2021",
+    description:
+      "Laguna Beach is a small coastal city in Orange County, California. It's known for its many art galleries, coves and beaches. Main Beach features tide pools and a boardwalk leading to the paths and gardens of nearby Heisler Park. Aliso Beach Park is a popular surf spot. The waters off Crystal Cove State Park are designated as an underwater park. Trails meander through coastal canyons in Laguna Coast Wilderness Park.",
+    imageUrl: "../public/yosemite.jpg",
+  },
+  {
+    id: 5,
+    title: "Los Angeles",
+    location: "California",
+    googleMapsUrl: "https://goo.gl/maps/xT3fe3FAQeVQtQP86",
+    startDate: "1 Jan, 2021",
+    endDate: "1 Jan, 2021",
+    description:
+      "Los Angeles is a sprawling Southern California city and the center of the nation's film and television industry.",
+    imageUrl: "../public/yosemite.jpg",
+  },
+  {
+    id: 6,
+    title: "Chicago",
+    location: "Illinois",
+    googleMapsUrl: "https://goo.gl/maps/41zSzvNEaPirSrPH9",
+    startDate: "1 Jan, 2021",
+    endDate: "1 Jan, 2021",
+    description:
+      "Chicago, on Lake Michigan in Illinois, is among the largest cities in the U.S. Famed for its bold architecture, it has a skyline punctuated by skyscrapers such as the iconic John Hancock Center, 1,451-ft. Willis Tower (formerly the Sears Tower) and the neo-Gothic Tribune Tower. The city is also renowned for its museums, including the Art Institute of Chicago with its noted Impressionist and Post-Impressionist works.",
+    imageUrl: "../public/yosemite.jpg",
+  },
+  {
+    id: 7,
+    title: "New York City",
+    location: "New York",
+    googleMapsUrl: "https://goo.gl/maps/KSTsvE12euDpZP6MA",
+    startDate: "1 Jan, 2021",
+    endDate: "1 Jan, 2021",
+    description:
+      "New York City comprises 5 boroughs sitting where the Hudson River meets the Atlantic Ocean. At its core is Manhattan, a densely populated borough that's among the world's major commercial, financial and cultural centers. Its iconic sites include skyscrapers such as the Empire State Building and sprawling Central Park. Broadway theater is staged in neon-lit Times Square.",
+    imageUrl: "../public/yosemite.jpg",
+  },
+  {
+    id: 8,
+    title: "Las Vegas",
+    location: "Nevada",
+    googleMapsUrl: "https://goo.gl/maps/k1ZzrU1xSYwxGvnM6",
+    startDate: "1 Jan, 2021",
+    endDate: "1 Jan, 2021",
+    description:
+      "Las Vegas, often known simply as Vegas, is the 25th-most populous city in the United States, the most populous city in the state of Nevada, and the county seat of Clark County. The city anchors the Las Vegas Valley metropolitan area and is the largest city within the greater Mojave Desert.",
+    imageUrl: "../public/yosemite.jpg",
+  },
+];
